e8abd91cb486ed450660afdfceeaa2ca89e200d3
Hari <two1821125run@protonmail.com>
2023-05-06 10:26:14 +0530

    initial setup - assets, config, ts.

---
+++ public/L.md
@@ -0,0 +1,4 @@
+### General
+
+- don't use decorative graphics as full backgrounds. use psuedo selectors instead.
+- don't use google fonts through link - use next/font instead
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      primary: "Josefin Sans",
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        lg: "0",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1110px",
+    },
     extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#062d3e",
+          hover: "#021f2C",
+        },
+        cream: "#ebe3cc",
+        teal: "#189cab",
+        orange: {
+          DEFAULT: "#ed5c01",
+          hover: "#e45a03",
+        },
+        yellow: "#fbbd08",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        hero: "url('/img/hero/hero-bg.png')",
+        pets: "url('/img/pets/pets-bg.png')",
+        services: "url('/img/services/bg.png')",
+        adoption: "url('/img/adoption/bg.png')",
+        newsletterYellow: "url('/img/newsletter/bg-yellow.svg')",
+        newsletterOrange: "url('/img/newsletter/bg-orange.svg')",
+        footer: "url('/img/footer/bg.svg')",
       },
     },
   },
